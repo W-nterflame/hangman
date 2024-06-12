b7958759037f2b921c142fbfa1447752084f47f1
Winterflame <95693999+W-nterflame@users.noreply.github.com>
2024-06-12 21:35:12 +0800

    Main class

---
+++ Main.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+		public void start(Stage primaryStage) {
+		MainMenu mainMenu = new MainMenu();
+		mainMenu.start(primaryStage);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
